9ac1350585390515f8a90f208e07416c0cfee093
Leonel Amaya <omar.mejia2099@gmail.com>
2023-08-07 16:38:50 -0500

    Implementando seguridad a la API

---
+++ src/main/java/com/foro/api/domain/dto/seguridad/DatosJWTToken.java
@@ -0,0 +1,4 @@
+package com.foro.api.domain.dto.seguridad;
+
+public record DatosJWTToken(String jwtToken) {
+}
--- src/main/java/com/foro/api/domain/model/Usuario.java
+++ src/main/java/com/foro/api/domain/model/Usuario.java
@@ -5,13 +5,19 @@ import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "Usuario")
 @Table(name = "usuarios1")
 @Getter
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,5 +29,40 @@ public class Usuario {
         this.nombre = usuarioDTO.nombre();
         this.email = usuarioDTO.email();
         this.contrasena = usuarioDTO.contrasena();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
